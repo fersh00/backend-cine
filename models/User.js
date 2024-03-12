28ef2716fe55616400aa3d638302c6c5e1788384
fc <fc323897@gmail.com>
2024-03-12 11:42:59 -0700

    Se crea modelo User.js

---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	DNI: {
+		type: String,
+		require: true,
+	},
+	name: {
+		type: String,
+		require: true,
+	},
+	lastname: {
+		type: String,
+		require: true,
+	},
+	dob: Number,
+	rol: {
+		type: String,
+		require: true,
+	},
+	phone: String,
+	email: String,
+	username: {
+		type: String,
+		require: true,
+	},
+	password: {
+		type: String,
+		require: true,
+	},
+
+});
+
+export default mongoose.model('User', userSchema);
