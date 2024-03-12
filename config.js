2d557375d141ba7f9744922ca9e111880e4c34e3
fc <fc323897@gmail.com>
2024-03-12 11:09:59 -0700

    Se modifica config.js para comprobacion de conexion a db

--- config.js
+++ config.js
@@ -1,3 +1,18 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+dotenv.config();
+
+const connect = () => {
+    mongoose.connect(process.env.DB_URI);
+    const connection = mongoose.connection;
+
+    connection.once('open', () => {
+        console.log('Conexion exitosa a db');
+    });
+    connection.once('error', () => {
+        console.error('Error de conexion a db');
+    });
+};
+
+export default connect;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import connect from './config.js';
+
+connect();
 
 const api = express();
+
 api.listen(8002, () => {
-	console.log("Hola desde puerto 8002");
+	console.log("Hola, desde puerto 8002");
 });
+
+
